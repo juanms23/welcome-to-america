932aba06d9dc99c5beeb5d95b7812b144c8b5ff2
Juan Carlos Muratalla-Sanchez <murataj2@wwu.edu>
2025-05-19 12:09:24 -0700

    Testing fonts, added new app icon

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="p-6 text-center font-[Merriweather]">
-      <h1 className="text-3xl font-[Merriweather] font-bold mb-4">Home</h1>
-      <p className="font-[Inter]">This is the homepage</p>
+      <h1 className="text-4xl font-[Merriweather] font-bold mb-4">Home</h1>
+      <p className="text-base font-[Inter]">This is the homepage</p>
     </div>
   );
 }
