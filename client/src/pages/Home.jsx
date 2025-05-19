2a123b85feacfe5460b89f5f9fa46f8105ebdc3a
Juan Carlos Muratalla-Sanchez <murataj2@wwu.edu>
2025-05-18 19:50:18 -0700

    Added fonts to .html and testing Tailwind in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold mb-4">Home</h1>
-      <p>This is the homepage</p>
+    <div className="p-6 text-center font-[Merriweather]">
+      <h1 className="text-3xl font-[Merriweather] font-bold mb-4">Home</h1>
+      <p className="font-[Inter]">This is the homepage</p>
     </div>
   );
 }
