4d635d12a432d8429506b9f3cd93446f1a32441a
JoeyPatricio <122133686+JoeyPatricio@users.noreply.github.com>
2025-05-24 15:50:41 -0700

    Added about page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import Topics from "./pages/Topics.jsx";
 import Forms from "./pages/Forms.jsx";
 import FindHelp from "./pages/FindHelp.jsx";
 import Registration from "./pages/Registration.jsx";
+import About from "./pages/About.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="forms" element={<Forms />} />
           <Route path="find_help" element={<FindHelp />} />
           <Route path="registration" element={<Registration />} />
+          <Route path="about" element={<About />} />
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
